523ab4613b3324e0250e33829c95ecf231080057
DmytroBozhor <dimaastop1@gmail.com>
2024-04-29 23:19:30 +0200

    transactional example

--- src/main/java/dmytro/bozhor/springbootdemo/service/PersonService.java
+++ src/main/java/dmytro/bozhor/springbootdemo/service/PersonService.java
@@ -2,8 +2,11 @@ package dmytro.bozhor.springbootdemo.service;
 
 import dmytro.bozhor.springbootdemo.Person;
 import dmytro.bozhor.springbootdemo.repository.PersonRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -15,6 +18,16 @@ public class PersonService {
 
     public Person save(Person person) {
         return personRepository.save(person);
+    }
+
+    @Transactional(readOnly = true)
+    /*@Transactional(
+            readOnly = true,
+            isolation = Isolation.SERIALIZABLE,
+            rollbackFor = Throwable.class,
+            propagation = Propagation.REQUIRES_NEW)*/
+    public Person findById(Long id) {
+        return personRepository.findById(id).orElseThrow(EntityNotFoundException::new);
     }
 
 }
