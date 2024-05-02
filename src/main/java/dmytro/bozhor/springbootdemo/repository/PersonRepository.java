19fe2409e27031f709b635e5d35ef873f9d04bc9
DmytroBozhor <dimaastop1@gmail.com>
2024-05-02 23:36:52 +0200

    person repository part tree jpa query example

--- src/main/java/dmytro/bozhor/springbootdemo/repository/PersonRepository.java
+++ src/main/java/dmytro/bozhor/springbootdemo/repository/PersonRepository.java
@@ -4,6 +4,18 @@ import dmytro.bozhor.springbootdemo.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
+
+    List<Person> findByNameContainingIgnoreCase(String name);
+
+    List<Person> findAllByAge(Integer age);
+
+    List<Person> findBySurnameContaining(String surname);
+
+    Optional<Person> findBySurname(String surname);
+
 }
